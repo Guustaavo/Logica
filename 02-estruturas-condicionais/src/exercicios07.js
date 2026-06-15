9b092d0eb5b930d1f834a3251690362fc5e7892d
GUSTAVO AMBRÓSIO GUIMARÃES <gustavo_guimaraes153@estudante.sesisenai.org.br>
2026-06-15 19:32:02 -0300

    exercicios 2

--- 02-estruturas-condicionais/src/exercicios07.js
+++ 02-estruturas-condicionais/src/exercicios07.js
@@ -1,1 +1,13 @@
 import leia from 'readline-sync';
+
+var salario = leia.questionFloat("Digite o valor do salario: ")
+var emprestimo = leia.questionFloat("Digite o valor do emprestimo: ")
+var prestacoes = leia.questionInt("Digite o numero de prestacoes: ")
+
+var valorPrestacao = emprestimo / prestacoes
+
+if(valorPrestacao <= salario * 0.30){
+    console.log("Emprestimo pode ser concedido.")
+}else{
+    console.log("Emprestimo nao pode ser concedido.")
+}
---
+++ 02-estruturas-condicionais/src/exercicios11.js
@@ -0,0 +1,16 @@
+import leia from 'readline-sync';
+
+var time1 = leia.questionInt("Digite a quantidade de gols feitos pelo primeiro time: ")
+var time2 = leia.questionInt("Digite a quantidade de gols feitos pelo segundo time: ")
+var diferenca = time1 - time2
+var diferenca2 = time2 - time1
+
+if(diferenca === 0){
+    console.log("Esse jogo foi um empate!!!")
+}else if(diferenca >= 1 && diferenca <= 3 || diferenca2 >= 1 && diferenca2 <= 3){
+    console.log("Esse jogo foi uma partida normal!")
+}else if(diferenca >= 4 || diferenca2 >= 4){
+    console.log("Esse jogo foi uma goleada!!!")
+}else{
+    console.log("Gols negativos?")
+}
