ce26a5c1f54612c064d532f11b4c590528d96b0f
GUSTAVO AMBRÓSIO GUIMARÃES <gustavo_guimaraes153@estudante.sesisenai.org.br>
2026-06-15 21:58:20 -0300

    bomb game

---
+++ minigames/src/bomba.js
@@ -0,0 +1,33 @@
+import leia from "readline-sync";
+
+/*
+var chances = 2
+var fios = leia.keyInSelect(["Fio Vermelho","Fio Verde", "Fio Amarelo","Fio Azul"],"Corte um fio! ")
+var bomba = fios[Math.floor(Math.random() * fios.lenght)];
+
+do {
+    if (fios === bomba) {
+        console.log("Você desarmou a bomba!");
+        break;
+    }
+
+    chances--;
+
+    if (chances > 0) {
+        console.log("Você errou o fio! Restaram apenas " + chances + " tentativa(s)");
+        fios = leia.keyInSelect(
+            ["Fio Vermelho","Fio Verde","Fio Amarelo","Fio Azul"],
+            "Corte um fio!"
+        );
+    }
+
+} while (fios !== bomba && chances > 0);
+
+if(fios !== bomba && chances === 0){
+    console.log("VAI EXPLODIR!!!")
+    console.log("---BOOMMMMMMMMMM---")
+//    console.log("O fio correto era o " + bomba + "!")
+}else(
+    console.log("VOCÊ CORTOU O FIO CORRETO!")
+)*/
+
