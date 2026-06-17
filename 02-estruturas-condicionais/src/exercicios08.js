3f199a7ec57fcd039c54f1de7e8e6a7d443fa609
GUSTAVO AMBRÓSIO GUIMARÃES <gustavo_guimaraes153@estudante.sesisenai.org.br>
2026-06-16 21:04:30 -0300

    novos exercicios

--- 02-estruturas-condicionais/src/exercicios08.js
+++ 02-estruturas-condicionais/src/exercicios08.js
@@ -3,7 +3,7 @@ import leia from 'readline-sync';
 var months = [
     "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
 ]
-var option = leia.keyInSelect(months, "Type the month")
+var option = leia.keyInSelect(months, "Select the month")
 
 if(months === "January" || months === "March" || months === "May" || months === "July" || months === "August" || months === "October" || months === "November" || months === "December"){
     console.log("This month have 31 days.")
---
+++ 02-estruturas-condicionais/src/parking.js
@@ -0,0 +1,3 @@
+import leia from 'readline-sync';
+
+var horas = leia.questionInt("Digite a quantidade de horas que o veiculo ficou no estacionamento: ")
