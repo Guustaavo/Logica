828188162a975720df919254ec016edb9e796869
Guustaavo <gustavo.guimaraes2148@gmail.com>
2026-08-26 15:57:14 -0300

    exercicio 9 estruturas de dados

--- 04-1-estruturas-de-dados/src/exercicios.js
+++ 04-1-estruturas-de-dados/src/exercicios.js
@@ -131,7 +131,7 @@ do pai e com as características genéticas ímpares da mãe*/
         pai[i] = randomPai;
         mae[i] = randomMae;
 
-  
+
         if (i % 2 === 0) {
             filho[i] = pai[i];
         }
@@ -176,72 +176,79 @@ export function exercicio09() {
     var itens = [];
     var temDuplicada = false;
 
-    for (i = 0; i < 10; i++) {
-        var random = Math.floor(Math.random() * 10) + 1;
+    for (var i = 0; i < 10; i++) {
+        var random = Math.floor(Math.random() * 32) + 1;
         itens[i] = random;
-        for (j = 0; j < itens.length; j++) {
-            for (let j = i + 1; j < itens.length; j++) {
-                if (itens[i] === itens[j]) {
-                    temDuplicado = true;
-                    break;
-                }
+    }
+
+    for (var i2 = 0; i2 < itens.length; i2++) {
+        for (var j = i2 + 1; j < itens.length; j++) {
+            if (itens[i2] === itens[j]) {
+                temDuplicada = true;
+                break;
             }
         }
     }
+
+    if (temDuplicada) {
+        console.log("Há um ou mais valores duplicados.")
+    } else {
+        console.log("Não há nenhum valor duplicado, resultado como esperado!")
+    }
 }
 
-        export function exercicio10() {
-            console.log("--- EXERCICIO 10 ---\n");
+export function exercicio10() {
+    console.log("--- EXERCICIO 10 ---\n");
+}
+
+export function exercicio11() {
+    console.log("--- EXERCICIO 11 ---\n");
+}
+
+export function exercicio12() {
+    /*Você está construíndo um algoritmo para verificar se um dado de jogo é viciado ou não.
+    Você deve “jogar” o dado 1000x (gerar números de 1 até 6) e verificar a quantidade e a
+    porcentagem de vezes que cada valor é gerado. Caso, algum dos valores seja gerado,
+    mais de 22% das vezes, você deve dizer que o dado é viciado, caso contrário, dizer que
+    ele não é viciado.*/
+    console.log("--- EXERCICIO 12 ---\n");
+
+    var lados = [0, 0, 0, 0, 0, 0];
+    var viciado = false;
+
+    for (var i = 0; i < 1000; i++) {
+        var resultadoDado = Math.floor(Math.random() * 6) + 1;
+        lados[resultadoDado - 1]++;
+        /* if(resultadoDado === 1) {
+            lados[0]++
+            }else if (resultadoDado === 2) {
+                lados[1]++
+                }else if (resultadoDado === 3) {
+                    lados[2]++
+                    }else if (resultadoDado === 4) {
+                        lados[3]++
+                        }else if (resultadoDado === 5) {
+                            lados[4]++
+                            }else if (resultadoDado === 6) {
+                                lados[5]++
+                                }*/
+    }
+    for (j = 0; j < lados.length; j++) {
+        var valor = lados[j];
+        var porcentagem = (valor / 1000) * 100;
+        if (porcentagem > 22) {
+            viciado = true;
         }
+    }
+    console.log("\nRESULTADO:")
+    if (viciado) {
+        console.log("O dado é viciado! Um ou mais lados caíram mais de 22% das vezes jogadas.")
+    }
+    else {
+        console.log("O dado não é viciado.")
+    }
+}
 
-        export function exercicio11() {
-            console.log("--- EXERCICIO 11 ---\n");
-        }
-
-        export function exercicio12() {
-            /*Você está construíndo um algoritmo para verificar se um dado de jogo é viciado ou não.
-            Você deve “jogar” o dado 1000x (gerar números de 1 até 6) e verificar a quantidade e a
-            porcentagem de vezes que cada valor é gerado. Caso, algum dos valores seja gerado,
-            mais de 22% das vezes, você deve dizer que o dado é viciado, caso contrário, dizer que
-            ele não é viciado.*/
-            console.log("--- EXERCICIO 12 ---\n");
-
-            var lados = [0, 0, 0, 0, 0, 0];
-            var viciado = false;
-
-            for (var i = 0; i < 1000; i++) {
-                var resultadoDado = Math.floor(Math.random() * 6) + 1;
-                lados[resultadoDado - 1]++;
-                /* if(resultadoDado === 1) {
-                    lados[0]++
-                    }else if (resultadoDado === 2) {
-                        lados[1]++
-                        }else if (resultadoDado === 3) {
-                            lados[2]++
-                            }else if (resultadoDado === 4) {
-                                lados[3]++
-                                }else if (resultadoDado === 5) {
-                                    lados[4]++
-                                    }else if (resultadoDado === 6) {
-                                        lados[5]++
-                                        }*/
-            }
-            for (j = 0; j < lados.length; j++) {
-                var valor = lados[j];
-                var porcentagem = (valor / 1000) * 100;
-                if (porcentagem > 22) {
-                    viciado = true;
-                }
-            }
-            console.log("\nRESULTADO:")
-            if (viciado) {
-                console.log("O dado é viciado! Um ou mais lados caíram mais de 22% das vezes jogadas.")
-            }
-            else {
-                console.log("O dado não é viciado.")
-            }
-        }
-
-        export function exercicio13() {
-            console.log("--- EXERCICIO 13 ---\n");
-        }
+export function exercicio13() {
+    console.log("--- EXERCICIO 13 ---\n");
+}
