224c452090fa0bc41b7b2b0b41287b550481b57b
Gustavo <gustavo.guimaraes2148@gmail.com>
2026-08-30 18:30:19 -0300

    sistema livraria finalizado

--- 04-2-estruturas-de-dados/src/exercicio4.js
+++ 04-2-estruturas-de-dados/src/exercicio4.js
@@ -46,3 +46,160 @@ var livros = [
         emprestado: false,
     }
 ]
+
+function listarLivros() {
+    var i = 1;
+    console.log("\n=== LIVROS ===\n");
+    livros.forEach((livro) => {
+        console.log(i);
+        console.log("Título: " + livro.titulo);
+        console.log("Autor: " + livro.autor);
+        console.log("Ano: " + livro.ano);
+        if (livro.emprestado === false) {
+            console.log("Livro disponível.");
+        }
+        else {
+            console.log("Livro indisponível.");
+        }
+        i++;
+    })
+}
+
+function mostrarLivrosDisponiveis() {
+    var i = 1;
+    console.log("\n=== LIVROS DISPONÍVEIS ===\n");
+    livros.forEach((livro) => {
+        if (livro.emprestado === false) {
+            console.log(i);
+            console.log("Título: " + livro.titulo);
+            console.log("Autor: " + livro.autor);
+            console.log("Ano: " + livro.ano);
+        }
+        i++;
+    })
+}
+
+function emprestarLivro() {
+    var opcoes = [];
+    livros.forEach((livro) => {
+        opcoes.push(livro.titulo);
+    })
+    console.log("\n=== OBTENHA UM LIVRO ===\n");
+    var escolha = leia.keyInSelect(opcoes, "Selecione um livro para pegar emprestado: ");
+    for (var i = 0; i < opcoes.length; i++) {
+        if (escolha === i) {
+            livros[i].emprestado = true;
+        }
+    }
+}
+
+function devolverLivro() {
+    var opcoes = [];
+    livros.forEach((livro) => {
+        opcoes.push(livro.titulo);
+    })
+    console.log("\n=== DEVOLVA UM LIVRO ===\n");
+    var escolha = leia.keyInSelect(opcoes, "Selecione o livro que deseja devolver: ");
+    for (var i = 0; i < opcoes.length; i++) {
+        if (escolha === i) {
+            livros[i].emprestado = false;
+        }
+    }
+}
+
+function mostrarLivroMaisAntigo() {
+    console.log("\n=== LIVRO MAIS ANTIGO ===\n");
+    var maisAntigo = livros[0].ano;
+    livros.forEach((livro) => {
+        if (livro.ano < maisAntigo) {
+            maisAntigo = livro.ano;
+        }
+    })
+    livros.forEach((livro) => {
+        if (livro.ano === maisAntigo) {
+            console.log("Título: " + livro.titulo);
+            console.log("Autor: " + livro.autor);
+            console.log("Ano: " + livro.ano);
+        }
+    })
+}
+
+function mostrarLivroMaisRecente() {
+    console.log("\n=== LIVRO MAIS RECENTE ===\n");
+    var maisRecente = livros[0].ano;
+    livros.forEach((livro) => {
+        if (livro.ano > maisRecente) {
+            maisRecente = livro.ano;
+        }
+    })
+    livros.forEach((livro) => {
+        if (livro.ano === maisRecente) {
+            console.log("Título: " + livro.titulo);
+            console.log("Autor: " + livro.autor);
+            console.log("Ano: " + livro.ano);
+        }
+    })
+}
+
+function buscarPorAutor() {
+    console.log("\n=== PESQUISAR AUTOR ===\n");
+    var autor = leia.question("Escreva o nome do autor: ");
+    livros.forEach((livro) => {
+        if (livro.autor.toLowerCase() === autor.toLowerCase()) {
+            console.log("Título: " + livro.titulo);
+            console.log("Autor: " + livro.autor);
+            console.log("Ano: " + livro.ano);
+        }
+    })
+}
+
+function continuar() {
+    var continuar = leia.keyInSelect(["Voltar"], "Confirme para voltar ao menu principal.");
+}
+
+var opcoes = [
+    "Listar livros",
+    "Mostrar livros disponíveis",
+    "Emprestar livro",
+    "Devolver livro",
+    "Mostrar livro mais antigo",
+    "Mostrar livro mais recente",
+    "Buscar livros por autor",
+    "Parar sistema"
+]
+
+do {
+    console.clear();
+    console.log("\n=== LIVRARIA ===");
+
+    var opcao = leia.keyInSelect(opcoes, "Selecione uma opção: ");
+
+    if (opcao === 0) {
+        listarLivros();
+        continuar();
+    }
+    if (opcao === 1) {
+        mostrarLivrosDisponiveis();
+        continuar();
+    }
+    if (opcao === 2) {
+        emprestarLivro();
+        continuar();
+    }
+    if (opcao === 3) {
+        devolverLivro();
+        continuar();
+    }
+    if (opcao === 4) {
+        mostrarLivroMaisAntigo();
+        continuar();
+    }
+    if (opcao === 5) {
+        mostrarLivroMaisRecente();
+        continuar();
+    }
+    if (opcao === 6) {
+        buscarPorAutor();
+        continuar();
+    }
+} while (opcao !== 7);
