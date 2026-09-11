880d172263abd0cb843881159457d62ee3ab25a5
Gustavo <gustavo.guimaraes2148@gmail.com>
2026-09-10 21:40:53 -0300

    extras estruturas de dados

---
+++ 04-2-estruturas-de-dados/src/extras/estacionamento.js
@@ -0,0 +1,49 @@
+import leia from 'readline-sync';
+/* 
+Crie um vetor contendo 15 objetos representando vagas de um estacionamento. Cada vaga deverá possuir:
+
+numero
+ocupada
+placa
+modelo
+
+O programa deverá apresentar o seguinte menu:
+1 - Mostrar vagas
+2 - Ocupar vaga
+3 - Liberar vaga
+4 - Mostrar quantidade de vagas livres
+5 - Mostrar quantidade de vagas ocupadas
+6 - Buscar veículo pela placa
+0 - Encerrar 
+
+Ao ocupar uma vaga, o usuário deverá informar o número da vaga, a placa e o modelo do veículo.
+Ao liberar uma vaga, o programa deverá percorrer o vetor procurando a vaga pelo número informado.
+Ao buscar um veículo, o programa deverá percorrer o vetor procurando pela placa informada.
+Não permita ocupar uma vaga que já esteja ocupada.
+Não permita liberar uma vaga que esteja livre.
+Ao final do programa, mostre a porcentagem de ocupação do estacionamento.
+*/
+
+var estacionamento = [];
+
+for (var i = 0; i < 15; i++) {
+    var vaga = {
+        numero: i + 1,
+        ocupada: false,
+        placa: "",
+        modelo: "",
+    }
+    estacionamento.push(vaga);
+}
+
+function mostrarVagas() {
+    estacionamento.forEach((vaga) => {
+        console.log("Vaga: " + vaga.numero);
+        console.log("Ocupada: " + vaga.ocupada);
+        console.log("\n");
+    })
+}
+
+function ocuparVaga() {
+    
+}
